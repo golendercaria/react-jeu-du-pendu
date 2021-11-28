7d2b579a5189f855e59e5ec135953b7adf90e5d6
golendercaria <>
2021-11-28 16:18:04 +0100

    Add build to git

---
+++ build/precache-manifest.53c8664f2be2f124473ec473d3ef37a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b1475854bb90d2a136d1bb23b08f4c7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c23d95b1b42b30ce9217",
+    "url": "./static/css/main.a31b0b6c.chunk.css"
+  },
+  {
+    "revision": "ee41355f6e012f016590",
+    "url": "./static/js/2.dce77e5b.chunk.js"
+  },
+  {
+    "revision": "c23d95b1b42b30ce9217",
+    "url": "./static/js/main.b3f6e0a8.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
